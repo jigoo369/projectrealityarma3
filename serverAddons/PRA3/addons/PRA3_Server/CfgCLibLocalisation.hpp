0c5817a7017da4e9cb3b5619d20bb82fb2f7d5c5
jigoo369 <jigoo369@gmail.com>
2016-11-02 19:28:03 -0400

    Add Korean
    Axion from Brotherhood without Banners Gaming Group.

--- serverAddons/PRA3/addons/PRA3_Server/CfgCLibLocalisation.hpp
+++ serverAddons/PRA3/addons/PRA3_Server/CfgCLibLocalisation.hpp
@@ -8,6 +8,7 @@ class CfgCLibLocalisation {
                 English = "New vehicle available: %1";
                 German = "Neues Fahrzeug verfügbar: %1";
                 Polish = "Nowy pojazd dostępny: %1";
+                Korean = "신규 장비 사용 가능: %1";
             };
         };
 
@@ -17,11 +18,14 @@ class CfgCLibLocalisation {
                 English = "You need wait %1 seconds until you can change sides again";
                 German = "Du musst %1 Sekunden warten bis du die Seite wieder wechseln kannst";
                 Polish = "Musisz poczekać %1 sekund przed ponowną zmianą strony";
+                Korean = "%1초 뒤에 세력을 변경하실 수 있습니다";
+                
             };
             class MaxPlayerCount {
                 English = "Maximum player count on the enemy side has been reached.";
                 German = "Maximale Spieleranzahl auf gegnerischen Seite erreicht.";
                 Polish = "Drużyna przeciwna osiągnęła maksymalną ilość graczy";
+                Korean = "상대측 세력의 인원이 다 찼습니다.";
             };
         };
 
@@ -31,6 +35,7 @@ class CfgCLibLocalisation {
                 English = "You have captured sector %1";
                 German = "Du hast Sektor %1 eingenommen";
                 Polish = "Zdobyłeś sektor %1";
+                Korean = "%1 지역을 점령하였습니다.";
                 
             };
 
@@ -38,22 +43,28 @@ class CfgCLibLocalisation {
                 English = "Your team has captured sector %1";
                 German = "Dein Team hat Sektor %1 eingenommen";
                 Polish = "Twoja drużyna zdobyła sektor %1";
+                Korean = "아군이 %1 지역을 점령하였습니다.";
             };
 
             class YouSL {
                 English = "You have lost sector %1";
                 German = "Dein Team hat Sektor %1 verloren";
                 Polish = "Utraciłeś sektor %1";
+                Korean = "%1 지역을 잃었습니다.";
+                
             };
             class YouSN {
                 English = "You have neutralized sector %1";
                 German = "Du hast Sektor %1 neutralisiert";
                 Polish = "Zneutralizowałeś sektor %1";
+                Korean = "%1 지역을 수복하였습니다.";
+                
             };
             class YourTSN {
                 English = "Your has team neutralized sector %1";
                 German = "Dein Team hat Sektor %1 neutralisiert";
                 Polish = "Twoja drużyna zneutralizowała sektor %1";
+                Korean = "아군이 %1 지역을 수복하였습니다.";
             };
         };
 
@@ -64,16 +75,20 @@ class CfgCLibLocalisation {
                 English = "You have to join a squad!";
                 German = "Du musst erst einem Squad beitreten!";
                 Polish = "Musisz dołączyć do oddziału!";
+                Korean = "분대에 참여해주십시오";
             };
             class ChooseARole {
                 English = "You have to select a role!";
                 German = "Du musst erst eine Rolle auswählen!";
                 Polish = "Musisz wybrać rolę!";
+                Korean = "보직을 선택해주십시오";
             };
             class selectSpawn {
                 English = "You have to select a spawnpoint!";
                 German = "Du musst erst einen Spawn Punkt auswählen!";
                 Polish = "Musisz wybrać punkt odrodzenia!";
+                Korean = "스폰포인트를 선택해주십시오";
+                
             };
         };
 
@@ -84,31 +99,39 @@ class CfgCLibLocalisation {
                 English = "Drag %1";
                 German = "Ziehe %1";
                 Polish = "Ciągnij %1";
+                Korean = "끌어오기";
             };
             class Drop {
                 English = "Drop";
                 German = "Loslassen";
                 Polish = "Upuść";
+                Korean = "내려놓기";
+                
             };
             class loadItem {
                 English = "Load item into %1";
                 German = "Lade Objekt in %1";
                 Polish = "Załaduj przedmiot %1";
+                Korean = "%1에 적재하기";
             };
             class noCargoSpace {
                 English = "No cargo space available";
                 German = "Kein Ladeplatz mehr frei";
                 Polish = "Brak wolnej przestrzeni ładunkowej";
+                Korean = "화물을 적재할 공간이 없습니다.";
             };
             class UnloadItem {
                 English = "Unload item out of %1";
                 German = "Entlade Objekt aus %1";
                 Polish = "Wyładuj przedmiot z %1";
+                Korean = "%1에서 화물내리기";
+                
             };
             class itemToHeavy {
                 English = "Item is %1kg too heavy";
                 German = "Das Objekt ist %1kg zu schwer";
                 Polish = "Przedmiot waży o %1kg za dużo";
+                Korean = "";
             };
         };
 
@@ -118,22 +141,27 @@ class CfgCLibLocalisation {
                 English = "%1 (%2 spawns remaining)";
                 German = "%1 (%2 Spawns übrig)";
                 Polish = "%1 (Pozostało %2 odrodzeń)";
+                Korean = "%1 (스폰 %2 남음)";
+                
             };
             // - Rally
             class CreateRally {
                 English = "Create Rally Point";
                 German = "Platziere Rally Punkt";
                 Polish = "Stwórz punkt zbiórki";
+                Korean = "랠리포인트 생성"; 
             };
             class cantPlaceRally {
                 English = "You can not place a rallypoint at this position";
                 German = "Du kannst hier keinen Rally Punkt platzieren";
                 Polish = "Nie możesz utworzyć punktu zbiórki w tym miejscu";
+                Korean = "여기에 랠리포인트를 생성할 수 없습니다.";
             };
             class RallyPlaced {
                 English = "Your Squadleader has placed a rally near %1";
                 German = "Dein Squadleader hat einen Rally nahe %1 erstellt";
                 Polish = "Dowódca drużyny ustalił punkt zbiórki w pobliżu %1";
+                Korean = "분대장이 %1 근처에 랠리포인트를 생성하였습니다.";
             };
 
             // - FOB
@@ -141,16 +169,19 @@ class CfgCLibLocalisation {
                 English = "Destroy FOB";
                 German = "FOB zerstören";
                 Polish = "Zniszcz FOB";
+                Korean = "전진기치 철거";
             };
             class PlaceFOB {
                 English = "Place FOB";
                 German = "FOB Platzieren";
                 Polish = "Stwórz FOB";
+                Korean = "전진기치 배치";
             };
             class FOBPlaced {
                 English = "Squad %1 has placed a FOB near %2";
                 German = "Squad %1 hat ein FOB nahe %1 platziert";
                 Polish = "Drużyna %1 utworzyła FOB w pobliżu %2";
+                Korean = "%1 분대가 %2 전진기지 근처에 배치되었습니다";
             };
 
         };
@@ -161,6 +192,7 @@ class CfgCLibLocalisation {
                 English = "You're not allowed to use this vehicle";
                 German = "Du darfst dieses Fahrzeug nicht benutzen";
                 Polish = "Nie możesz używać tego pojazdu";
+                Korean = "귀관은 해당 차량을 사용할 수 없습니다.";
             };
         };
     };
